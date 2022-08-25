c4cb687923dcd08b7333754a3bc12ff087702136
dina4411 <112100148+dina4411@users.noreply.github.com>
2022-08-25 18:00:12 +0200

    Update commands.sql
    done 4 tasks

--- commands.sql
+++ commands.sql
@@ -1,1 +1,9 @@
-SELECT * FROM country WHERE Name LIKE "Rus%";
+-- 1. посмотреть все доступные БД
+SHOW DATABASES;
+-- 2. подключиться к БД "world" 
+USE world;
+-- 3. получить все данные из таблицы city
+SELECT * FROM city;
+-- 4. вывести только столбцы с названием стран и населением из таблицы city 
+SELECT CountryCode, Population FROM city
+-- 5. получить уникальные значения поля Language из таблицы countrylanguage 
