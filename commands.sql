52787e61b7c855a6c89f50382fd7325c5986c61c
dina4411 <112100148+dina4411@users.noreply.github.com>
2022-08-26 09:39:00 +0200

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -7,3 +7,24 @@ SELECT * FROM city;
 -- 4. вывести только столбцы с названием стран и населением из таблицы city 
 SELECT CountryCode, Population FROM city
 -- 5. получить уникальные значения поля Language из таблицы countrylanguage 
+SELECT DISTINCT Language FROM countrylanguage;
+-- 6. вывести города, где население превышает 5 000 000 человек из таблицы city 
+SELECT Name FROM city WHERE Population > 5000000;
+-- 7. вывести информацию о конкретном городе - "Лондон" из таблицы city
+SELECT * FROM city WHERE Name = "London";
+-- 8. узнать все официальные языки, где процент говорящих выше 80 % из таблицы countrylanguage
+SELECT * FROM countrylanguage WHERE Percentage >= 80;
+-- 9. узнать все города из Австралии или из России из таблицы city 
+SELECT * FROM city WHERE CountryCode IN ("AUS", "RUS");
+-- 10. узнать все города, которые НЕ находятся в Китае из таблицы city
+SELECT * FROM city WHERE NOT CountryCode = "CHN";
+-- 11. узнать данные, где язык = Английский или код страны НЕ “CAN” и процент говорящих НИ меньше 50 из таблицы countrylanguage
+-- составила 2 запроса, не понимаю, в чем разница. Как по мне, они оба подходят под условия, хотя дают разный результат:
+SELECT * FROM countrylanguage WHERE Language = "English" OR (CountryCode != "CAN" AND Percentage > "50");
+SELECT * FROM countrylanguage WHERE Language = "English" OR NOT (CountryCode = "CAN" AND Percentage < "50");
+-- 12. получить данные только определенных языков: Русский и Английский из таблицы countrylanguage 
+SELECT * FROM countrylanguage WHERE Language IN ("English", "Russian");
+-- 13. получить все города, населения которых от 1 000 000 до 5 000 000 из таблицы city 
+SELECT * FROM city WHERE Population BETWEEN 1000000 AND 5000000;
+-- 14. узнаем все страны с буквами "St" в названии
+SELECT * FROM country WHERE Name LIKE "%St%";
